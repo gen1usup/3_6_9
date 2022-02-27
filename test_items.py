4ffd94c7aafebf7d00d9dc36a55a8191af9d2506
gen1usup <60182513+gen1usup@users.noreply.github.com>
2022-02-27 20:43:41 +0300

    изменено название тестового метода

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link_pass(browser):
+def test_add_to_busket_button(browser):
     browser.get(link)
     time.sleep(30)
     busket_button = browser.find_element_by_xpath(
