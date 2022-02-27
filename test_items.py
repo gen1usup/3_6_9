a443e016fb291d0e9d13ad46ca900cfe0c6baee8
gen1usup <60182513+gen1usup@users.noreply.github.com>
2022-02-27 20:40:19 +0300

    изменен таймаут на 30

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_login_link_pass(browser):
     browser.get(link)
-    time.sleep(10)
+    time.sleep(30)
     busket_button = browser.find_element_by_xpath(
         '//button[@class="btn btn-lg btn-primary btn-add-to-basket" and @type="submit"]')
     assert busket_button is not None
